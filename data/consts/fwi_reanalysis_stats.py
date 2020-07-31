2c0fb1423993331e2b49da79f313087f36b9813a
tejasvi <45873379+tejasvi@users.noreply.github.com>
2020-07-31 18:01:19 +0000

    Add reanalysis stats for two month peak period

--- data/consts/fwi_reanalysis_stats.py
+++ data/consts/fwi_reanalysis_stats.py
@@ -6,6 +6,13 @@ REANALYSIS_FWI_VAR = 621.65894
 REANALYSIS_FWI_MIN = 1.7216163e-09
 REANALYSIS_FWI_MAX = 304.7236
 
+# Statistics for FWI reanalysis during Australian bushfire peak: December-January
+PEAKED_FWI_MEAN = 40.742332
+PEAKED_FWI_MAD = 20.210274
+PEAKED_FWI_VAR = 592.4607
+PEAKED_FWI_MIN = 10.000005
+PEAKED_FWI_MAX = 230.67097
+
 # The variance of FWI reanalysis calculated after applying the
 # box-cox transformation
 PROCESSED_REANALYSIS_FWI_VAR = 10.813191
