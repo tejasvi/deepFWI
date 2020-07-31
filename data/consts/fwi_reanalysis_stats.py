4eddaabdd53a792e39b13236ed610ad7839eadbe
tejasvi <45873379+tejasvi@users.noreply.github.com>
2020-07-31 18:57:03 +0000

    More specific stats description

--- data/consts/fwi_reanalysis_stats.py
+++ data/consts/fwi_reanalysis_stats.py
@@ -6,7 +6,7 @@ REANALYSIS_FWI_VAR = 621.65894
 REANALYSIS_FWI_MIN = 1.7216163e-09
 REANALYSIS_FWI_MAX = 304.7236
 
-# Statistics for FWI reanalysis during Australian bushfire peak: December-January
+# Statistics for FWI > 10 during Australian bushfire peak: December-January
 PEAKED_FWI_MEAN = 40.742332
 PEAKED_FWI_MAD = 20.210274
 PEAKED_FWI_VAR = 592.4607
