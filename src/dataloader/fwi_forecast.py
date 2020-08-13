dd0afe8ac99ddd458de17e4980a3e2293788a5e8
tejasvi <45873379+tejasvi@users.noreply.github.com>
2020-08-13 15:09:26 +0000

    Modify preprocess for benchmarking

--- src/dataloader/fwi_forecast.py
+++ src/dataloader/fwi_forecast.py
@@ -36,26 +36,17 @@ class ModelDataset(BaseDataset):
             forecast_dir=forecast_dir, hparams=hparams, **kwargs,
         )
 
-        self.hparams.thresh = self.hparams.out_mad / 2
-
-        # Consider only ground truth and discard forecast values
-        preprocess = lambda x: x.isel(time=slice(0, 1))
-
-        inp_files = sorted(
-            sorted(glob(f"{forcings_dir}/ECMWF_FO_2019*.nc")),
-            # Extracting the month and date from filenames to sort by time.
-            key=lambda x: int(x.split("2019")[1].split("_1200_hr_")[0][:2]) * 100
-            + int(x.split("2019")[1].split("_1200_hr_")[0][2:]),
-        )[:736]
-        inp_invalid = lambda x: not (
-            1 <= int(x.split("2019")[1].split("_1200_hr_")[0][:2]) <= 12
-            and 1 <= int(x.split("2019")[1].split("_1200_hr_")[0][2:]) <= 31
+        # Create new `lead` dimension to avoid duplication along `time` dimension
+        preprocess = (
+            lambda x: x.rename_dims(time="lead")
+            .rename_vars(name_dict={"time": "lead"})
+            .expand_dims("time")
+            .assign_coords(time=("time", [x.time[0].values]))
+            .assign_coords(lead=("lead", range(10)))
         )
-        # Checking for valid date format
-        assert not (sum([inp_invalid(x) for x in inp_files])), (
-            "Invalid date format for input file(s)."
-            "The dates should be formatted as YYMMDD."
-        )
+
+        out_files = glob(f"{forecast_dir}/ECMWF_FWI_20*_1200_hr_fwi.nc")
+
         with xr.open_mfdataset(
             inp_files, preprocess=preprocess, engine="h5netcdf"
         ) as ds:
